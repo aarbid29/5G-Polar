96a9a8189d3f70b3967e3b4bb841594a0d957ae8
baelthebard42 <078bct012.anjal@pcampus.edu.np>
2025-11-28 12:18:44 +0545

    added evaluation methods for model: bit error rates for frozen and non frozen bits, works well

--- src/dataset.py
+++ src/dataset.py
@@ -10,17 +10,20 @@ DATASET_PATH = '../scripts/data_32bits_polar.csv'
 
 class PolarDecDataset(Dataset):
 
-    def __init__(self, snr_db,  num_samples, snr_noise_std=0.1, transform = None):
+    def __init__(self, snr_db,  num_samples, snr_noise_std=0.1, fixed_msg_bit_size = None, transform = None):
         super().__init__()
         self.snr_db = snr_db + np.random.normal(0, snr_noise_std)
         self.num_samples = num_samples
+        self.fixed_msg_bit_size = fixed_msg_bit_size
         
 
     def __len__(self):
         return self.num_samples
         
     def __getitem__(self, idx):
-         channel_observation_vector, frozen_bit_prior, target = generate_data(message_bit_size=np.random.choice([8, 16, 24]), SNRs_db=[self.snr_db])
+         
+         
+         channel_observation_vector, frozen_bit_prior, target = generate_data(message_bit_size=np.random.choice([8, 16, 24]) if self.fixed_msg_bit_size is None else self.fixed_msg_bit_size, SNRs_db=[self.snr_db])
          channel_tensor = torch.tensor(channel_observation_vector, dtype=torch.float32)
          frozen_tensor = torch.tensor(frozen_bit_prior, dtype=torch.float32)
          snr_tensor = torch.tensor(self.snr_db, dtype=torch.float32)
